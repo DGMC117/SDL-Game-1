013165ab73459e0b0c0352455ab04dc5354f1b86
DGMC117 <dgarciadm@gmail.com>
2022-10-23 16:08:02 +0200

    Adds: ModuleScene.h and ModuleScene.cpp with basic framework for the module.

---
+++ ModuleScene.cpp
@@ -0,0 +1,40 @@
+#include "Globals.h"
+#include "ModuleScene.h"
+
+ModuleScene::ModuleScene()
+{
+}
+
+ModuleScene::~ModuleScene()
+{
+}
+
+bool ModuleScene::Init()
+{
+	return true;
+}
+
+bool ModuleScene::Start()
+{
+	return true;
+}
+
+update_status ModuleScene::PreUpdate()
+{
+	return UPDATE_CONTINUE;
+}
+
+update_status ModuleScene::Update()
+{
+	return UPDATE_CONTINUE;
+}
+
+update_status ModuleScene::PostUpdate()
+{
+	return UPDATE_CONTINUE;
+}
+
+bool ModuleScene::CleanUp()
+{
+	return true;
+}
---
+++ ModuleScene.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Module.h"
+#include "Globals.h"
+
+class ModuleScene : public Module
+{
+public:
+	ModuleScene();
+	~ModuleScene();
+
+	bool Init();
+	bool Start();
+	update_status PreUpdate();
+	update_status Update();
+	update_status PostUpdate();
+	bool CleanUp();
+};
